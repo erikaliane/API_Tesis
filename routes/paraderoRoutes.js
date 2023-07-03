6fcd1b3db7f3a89198375cc4100547b095d68c1c
ErikaVentura <105476284+erikaliane@users.noreply.github.com>
2023-07-02 20:46:56 -0500

    update paradero

--- routes/paraderoRoutes.js
+++ routes/paraderoRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const paraderoController = require('../controllers/paraderoController');
+const paraderoController = require('../controllers/ParaderoController');
 
 // Ruta para crear un paradero
 router.post('/', paraderoController.crearParadero);
